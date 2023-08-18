adedbb21b88fe9358020e0eccc19d9a70e01da37
Kate-os <c.ososova@gmail.com>
2023-08-18 18:25:26 +0500

    Задание 4.3.7

---
+++ src/Module4.3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите своё имя");
+
+var name = Console.ReadLine();
+
+Console.WriteLine("Ваше имя в обратном порядке: ");
+
+for (var i = name!.Length; i > 0; i--)
+{
+    Console.Write(name[i - 1] + " ");
+}
+
+Console.ReadKey();
+
